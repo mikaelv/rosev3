5f793591f91f2ead02a58f1b5b8b268d2d77a507
Mikael Valot <mikael.valot@gmail.com>
2017-04-19 22:15:26 +0100

    SLAM is working, saved default rviz config

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # rosev3
 ROS Docker containers for controlling a Lego EV3 robot
+
 See instructions here: https://www.instructables.com/id/ROS-Robot-With-Lego-EV3-and-Docker/
--- r200/run.sh
+++ r200/run.sh
@@ -2,5 +2,5 @@ basedir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ../master
 docker-compose up -d master
 cd $basedir
-docker-compose up 
-#docker exec -it r200 bash
+docker-compose up -d r200
+docker exec -it r200 bash
